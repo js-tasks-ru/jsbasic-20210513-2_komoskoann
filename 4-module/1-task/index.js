5efd43e2c567f7a85053a8bd83d8f129a218baaa
komoskoann\\----------- <komoskoann@gmail.com>
2021-05-30 17:42:18 +0300

    Solve 4 module tasks

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  
+  let list = document.createElement('ul');
+
+  for (let i = 0; i < friends.length; i++) {
+    
+    let item = document.createElement('li');
+    item.appendChild(document.createTextNode(friends[i].firstName + ' ' + friends[i].lastName));
+    list.appendChild(item);
+    
+  }
+return list;
 }
