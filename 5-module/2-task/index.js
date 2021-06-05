9c80ad8a29f03b4edb24ad859992f31b4feb329e
komoskoann\\----------- <komoskoann@gmail.com>
2021-06-05 18:30:39 +0300

    Solve 5 module tasks

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let elem = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+
+  elem.addEventListener('click', function() {
+    if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');
+    } else {
+    text.setAttribute('hidden', 'hidden');
+    }
+  });
 }
